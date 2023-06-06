f4e14ba8d95008156a957b978a70f67c862445c6
Murat Yilmaz <135061655+muratyilmaz729@users.noreply.github.com>
2023-06-06 10:24:01 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  5 15:22:39 2023
+
+@author: johan
+"""
+import medical_data_visualizer
+
+# Test your function by calling it here
+medical_data_visualizer.draw_cat_plot()
+medical_data_visualizer.draw_heat_map()
+
